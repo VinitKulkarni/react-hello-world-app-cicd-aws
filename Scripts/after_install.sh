f7c5623a3a6dc814e58172ba1dc6192569092752
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2024-09-17 14:31:40 +0530

    Update after_install.sh

--- Scripts/after_install.sh
+++ Scripts/after_install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-#_Change_Working_Directory
-cd /home/ubuntu/myproject
-
 #Install_node_modules_&_Make_React_Build
-npm  install
-npm run build
+sudo cp -r /home/ubuntu/myproject/build/* /var/www/helloworld/
